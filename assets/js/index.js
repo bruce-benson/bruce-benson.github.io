03b7c60124a1b5ca0480cc91096c0caabade1ce0
epainter <epainter@deloitte.com>
2019-05-13 23:41:37 -0700

    add new projects

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,16 +9,32 @@ document.addEventListener("DOMContentLoaded", function() {
     var hamburger = {
       navToggle: document.querySelector('.nav-toggle'),
       nav: document.querySelector('.nav'),
-  
+
       doToggle: function(e) {
         e.preventDefault();
         this.navToggle.classList.toggle('expanded');
         this.nav.classList.toggle('expanded');
       }
     };
-  
-    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-    // hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
-  
+
+    if(hamburger.navToggle) {
+      hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+    }
+
+    var imageLightBox = document.querySelectorAll('.project-image');
+
+    if (imageLightBox.length) {
+      imageLightBox.forEach(function(img) {
+        img.addEventListener('click', function () {
+          var src = this.src;
+          var instance = basicLightbox.create(`
+            <img src="` + src + `" width="800" height="600">
+          `);
+
+          instance.show();
+        });
+      });
+    }
+
   }());
 });
